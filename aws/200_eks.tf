1c5d765dee2963ce1a343f918e8e402d40d24d23
hkh8308kevin <hkh8308@gmail.com>
2025-03-29 14:33:57 +0900

    init

---
+++ aws/200_eks.tf
@@ -0,0 +1,149 @@
+## Create eks cluster
+data "aws_caller_identity" "current" {}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.29.0"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  # EBS 관련 정책 추가
+  iam_role_additional_policies = {
+    AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+    AmazonEC2FullAccess      = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  }
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      cluster_name = var.cluster_name
+      most_recent  = true
+    }
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_irsa.iam_role_arn
+    }
+    eks-pod-identity-agent = {
+      most_recent = true
+    }
+  }
+  enable_cluster_creator_admin_permissions = true
+  vpc_id                                   = aws_vpc.vpc.id
+  subnet_ids                               = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-c.id]
+
+  # EKS Managed Node Group
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.medium"]
+  }
+
+  eks_managed_node_groups = {
+    green = {
+      min_size     = 2
+      max_size     = 5
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+      iam_role_additional_policies = {
+        # AWS 관리형 정책 추가
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+    }
+
+    blue = {
+      name           = "blue-node-group" # 👈 새로운 노드 그룹 이름
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 1
+      instance_types = ["t3.large"]
+
+      iam_role_additional_policies = {
+        AmazonEC2ReadOnlyAccess = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+      }
+    }
+  }
+}
+
+module "ebs_csi_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.30"
+
+  role_name = "${var.cluster_name}-ebs-csi-controller"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
+module "vpc_cni_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 4.12"
+
+  role_name_prefix      = "VPC-CNI-IRSA"
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
+    common = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
+  }
+}
+
+############################################################################################
+## 로드밸런서 콘트롤러 설정
+## EKS 에서 Ingress 를 사용하기 위해서는 반듯이 로드밸런서 콘트롤러를 설정 해야함.
+## 참고 URL : https://docs.aws.amazon.com/ko_kr/eks/latest/userguide/aws-load-balancer-controller.html
+############################################################################################
+
+######################################################################################################################
+# 로컬변수
+# 쿠버네티스 추가 될때마다 lb_controller_iam_role_name 을 추가해야함.
+######################################################################################################################
+
+# locals {
+#   # eks 를 위한 role name
+#   k8s_aws_lb_service_account_namespace = "kube-system"
+#   lb_controller_service_account_name   = "aws-load-balancer-controller"
+# }
+
+######################################################################################################################
+# EKS 클러스터 인증 데이터 소스 추가
+######################################################################################################################
+
+data "aws_eks_cluster_auth" "eks_cluster_auth" {
+  name = var.cluster_name
+}
+
+# Load Balancer Controller를 위한 IAM Role 생성
+module "lb_controller_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name = "eks-aws-lb-controller-role"
+
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
